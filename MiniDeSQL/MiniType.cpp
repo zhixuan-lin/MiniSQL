ddc6dc5a5315d17715620aede7d50cfe260039c9
765743262 <765743262@qq.com>
2018-06-13 21:23:40 +0800

    conflict resolved

---
+++ MiniDeSQL/MiniType.cpp
@@ -0,0 +1,390 @@
+
+#include <string>
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <map>
+#include "MiniType.h"
+
+namespace MINI_TYPE
+{
+    // SqlValueType
+    int SqlValueType::BPTreeDegree() const
+    {
+        return static_cast<int>(BlockSize / (TypeSize() + sizeof(int)));
+    }
+    
+    size_t SqlValueType::TypeSize() const
+    {
+        switch(type)
+        {
+            case MiniInt: return sizeof(int);
+            case MiniFloat: return sizeof(float);
+            case MiniChar: return char_size;
+        }
+    }
+    
+    bool SqlValueType::operator==(const SqlValueType &s) const {
+        return s.type == type;
+    }
+
+    bool SqlValueType::operator!=(const SqlValueType &s) const {
+        return s.type != type;
+    }
+    std::ostream &operator<<(std::ostream &out, const SqlValueType & sqlValueType){
+        out << sqlValueType.type << ' ' << sqlValueType.char_size;
+        return out;
+    }
+
+    std::istream &operator>>(std::istream &in, SqlValueType &sqlValueType) {
+        int sqlValueTypeInt = 0;
+        in >> sqlValueTypeInt;
+        switch(sqlValueTypeInt) {
+            case MiniInt  : sqlValueType.type = MiniInt;   break;
+            case MiniFloat: sqlValueType.type = MiniFloat; break;
+            case MiniChar : sqlValueType.type = MiniChar;  break;
+            default:        sqlValueType.type = MiniInt;   break;
+        }
+        
+        in >> sqlValueType.char_size;
+        return in;
+    }
+   
+    
+    // SqlValue
+    void SqlValue::ReadFromMemory(const char * source, int byte_offset)
+    {
+        size_t size = type.TypeSize();
+        char temp[MaxChar + 1];
+        switch(type.type)
+        {
+            case MiniInt: std::memcpy(&i, source + byte_offset, size); break;
+            case MiniFloat: std::memcpy(&f, source + byte_offset, size); break;
+            case MiniChar:
+                std::memset(temp, 0, MaxChar + 1);
+                std::memcpy(temp, source + byte_offset, size);
+                str = temp;
+                break;
+        }
+    }
+    void SqlValue::WriteToMemory(char * dest, int byte_offset) const
+    {
+        size_t size = type.TypeSize();
+        switch(type.type)
+        {
+            case MiniInt: std::memcpy(dest + byte_offset, &i, size); break;
+            case MiniFloat: std::memcpy(dest + byte_offset, &f, size); break;
+            case MiniChar:
+                char temp[MINI_TYPE::MaxChar + 1];
+                std::memset(temp, 0, MINI_TYPE::MaxChar + 1);
+                std::memcpy(temp, str.c_str(), str.size());
+                std::memcpy(dest + byte_offset, temp, size); break;
+        }
+    }
+    std::string SqlValue::ToStr() const
+    {
+        switch(type.type)
+        {
+            case MiniInt: return std::to_string(i);
+            case MiniFloat: return std::to_string(f);
+            case MiniChar: return str;
+            default:
+                std::cerr << "Unknown Type!\n";
+                std::exit(0);
+        }
+    }
+    bool SqlValue::operator<(const SqlValue & s) const
+    {
+        switch(type.type)
+        {
+            case MiniInt: return i < s.i;
+            case MiniFloat: return f < s.f;
+            case MiniChar: return str < s.str;
+            default:
+                std::cerr << "Unknown Type!\n";
+                std::exit(0);
+        }
+    }
+    bool SqlValue::operator<=(const SqlValue & s) const
+    {
+        switch(type.type)
+        {
+            case MiniInt: return i <= s.i;
+            case MiniFloat: return f <= s.f;
+            case MiniChar: return str <= s.str;
+            default:
+                std::cerr << "Unknown Type!\n";
+                std::exit(0);
+        }
+    }
+    bool SqlValue::operator>(const SqlValue & s) const
+    { return not (*this <= s); }
+    bool SqlValue::operator>=(const SqlValue & s) const
+    { return not (*this < s); }
+    bool SqlValue::operator==(const SqlValue & s) const
+    { return *this <= s and *this >= s; }
+    bool SqlValue::operator!=(const SqlValue & s) const
+    { return not (*this == s); }
+    
+    
+    std::ostream &operator<<(std::ostream &out, const SqlValue & value)
+    {
+        switch(value.type.type)
+        {
+            case TypeId::MiniInt:
+                out << value.i;
+                break;
+            case TypeId::MiniFloat:
+                out << value.f;
+                break;
+            case TypeId::MiniChar:
+                out << value.str;
+                break;
+            default:
+                std::cerr << "Unknown Type!\n";
+                exit(0);
+        }
+        return out;
+    }
+    
+    
+    // TableInfo
+    TableInfo::TableInfo(const std::string nm, const std::vector<Attribute> attrs)
+    : name(nm), attributes(attrs)
+    {
+        for (const auto & attr : attrs)
+            record_length += attr.type.TypeSize();
+    }
+    
+    Attribute TableInfo::FetchAttribute(const std::string & attribute_name)
+    {
+        for (const auto & attr : attributes)
+        {
+            if (attr.name == attribute_name)
+                return attr;
+        }
+        std::cerr << "Attribute " + attribute_name + " does not exists!\n";
+        std::exit(0);
+    }
+    
+    std::ostream &operator<<(std::ostream &out, const TableInfo tableInfo) {
+        
+        out << tableInfo.name << ' '
+        << tableInfo.record_count << ' '
+        << tableInfo.record_length << ' '
+        << tableInfo.attributes.size() << ' '
+        << tableInfo.indices.size() << ' ';
+        
+        for (auto &attribute : tableInfo.attributes)
+            out << attribute << ' ';
+        
+        for (auto &index : tableInfo.indices)
+            out << index << ' ';
+        
+        out << std::endl;
+        return out;
+    }
+    
+    std::istream &operator>>(std::istream &in, TableInfo &tableInfo) {
+        int attrSize = 0;
+        int idxSize = 0;
+
+        in >> tableInfo.name >> tableInfo.primaryKey >> tableInfo.record_count >> tableInfo.record_length >> attrSize
+           >> idxSize;
+
+        for (int i = 0; attrSize > i; i++) {
+            Attribute attribute;
+            in >> attribute;
+            tableInfo.attributes.push_back(attribute);
+        }
+
+        for (int i = 0; idxSize > i; i++) {
+            std::pair<std::string, std::string> index;
+            in >> index;
+            tableInfo.indices.insert(index);
+        }
+        return in;
+    }
+    
+    // Record
+    Record Record::Extract(const std::vector<int> indices) const
+    {
+        Record result;
+        for (auto idx : indices)
+            result.values.push_back(values[idx]);
+        return result;
+    }
+    Record Record::Extract(int i) const
+    {
+        Record result;
+        result.values.push_back(values[i]);
+        return result;
+    }
+    Record Record::Extract(const TableInfo & table, const std::string & attribute) const
+    {
+        return Extract(table, std::vector<std::string>(1, attribute));
+    }
+    Record Record::Extract(const TableInfo & table, const std::vector<std::string> & attributes) const
+    {
+        std::vector<int> indices;
+        for (const auto & attr : attributes)
+        {
+            bool found = false;
+            for (int i = 0; i < table.attributes.size(); i++)
+            {
+                
+                if (table.attributes[i].name == attr)
+                {
+                    indices.push_back(i);
+                    found = true;
+                }
+            }
+            if (not found)
+            {
+                std::cerr << "Attribute " + attr + " does not exists!\n";
+                std::exit(0);
+            }
+        }
+        
+        return Extract(indices);
+    }
+    void Record::Conform(const TableInfo & table)
+    {
+        values.resize(table.attributes.size());
+        for (int i = 0; i < table.attributes.size(); i++)
+            values[i].type = table.attributes[i].type;
+    }
+    
+    std::ostream &operator<<(std::ostream &out, const Record & record)
+    {
+        for (const auto & value : record.values)
+            out << value << " ";
+        out << std::endl;
+        return out;
+    }
+    
+    // Attribute
+    
+    std::ostream &operator<<(std::ostream &out, const Attribute & attribute){
+        out << attribute.name << ' ' << attribute.type << ' ' << attribute.primary << ' ' << attribute.unique;
+        return out;
+    }
+    
+    std::istream &operator>>(std::istream &in, Attribute &attribute){
+        in >> attribute.name >> attribute.type >> attribute.primary >> attribute.unique;
+        return in;
+    }
+
+    // IndexInfo
+    std::ostream &operator<<(std::ostream &out, const std::pair<std::string, std::string> index){
+        out << index.first << ' ' << index.second;
+        return out;
+    }
+
+    std::ostream &operator<<(std::ostream &out, const IndexInfo & indexInfo) {
+        out << indexInfo.name << ' ' << indexInfo.attribute << ' ' << indexInfo.table << ' ' << indexInfo.alias << std::endl;
+        return out;
+    }
+
+    std::istream &operator>>(std::istream &in, IndexInfo & indexInfo) {
+        in >> indexInfo.name >> indexInfo.attribute >> indexInfo.table >> indexInfo.alias;
+        return in;
+    }
+    
+    std::istream &operator>>(std::istream &in, std::pair<std::string, std::string> &index){
+        in >> index.first >> index.second;
+        return in;
+    }
+
+    // Table
+    Table Table::Select(const std::vector<Condition> & conditions)
+    {
+        Table result(this->info);
+        for (const auto & record : records)
+            if (Test(conditions, this->info, record))
+                result.records.push_back(record);
+        return result;
+    }
+    
+    void Table::DisplayAttr(std::vector<std::string> attrList) {
+        std::vector<int> attrIdx;
+
+        for (int i = 0; info.attributes.size() > i; i++)
+            if (std::find(attrList.begin(), attrList.end(), info.attributes[i].name) != attrList.end()) {
+                attrIdx.push_back(i);
+                std::cout << info.attributes[i].name << '\t';
+            }
+
+        std::cout << std::endl;
+
+        for (auto &record : records) {
+            for (auto &idx : attrIdx) {
+                std::cout << record.values[idx].ToStr() << '\t';
+            }
+            std::cout << std::endl;
+        }
+    }
+    
+    std::ostream &operator<<(std::ostream &out, const Table & table) {
+
+        for (auto& attr : table.info.attributes)
+            out << attr.name << '\t';
+
+        out << std::endl;
+
+        for (auto& record : table.records) {
+            for (auto &value : record.values) {
+                out << value.ToStr() << '\t';
+            }
+            out << std::endl;
+        }
+
+        return out;
+    }
+
+    // Condition
+    bool Condition::Test(SqlValue val) const
+    {
+        switch (op)
+        {
+            case Operator::Equal: return val == value;
+            case Operator::GreaterThan: return val > value;
+            case Operator::GreaterEqual: return val >= value;
+            case Operator::LessThan: return val < value;
+            case Operator::LessEqual: return val <= value;
+            case Operator::NotEqual: return val != value;
+        }
+    }
+    bool Test(const std::vector<Condition> & conditions, const TableInfo & table, const Record & record)
+    {
+        for (const auto & cond : conditions)
+            if (not cond.Test(record.Extract(table, cond.attribute.name).values[0]))
+                return false;
+        return true;
+    }
+    bool Test(Condition & condition, const TableInfo & table, const Record & record)
+    {
+        if (not condition.Test(record.Extract(table, condition.attribute.name).values[0]))
+            return false;
+        return true;
+    }
+    
+    
+    
+    
+    
+    // Operator
+    Operator GetOpFromString(const std::string opStr) {
+        static std::map<std::string, Operator> toOp = {
+            {"=",  Equal},
+            {"!=", NotEqual},
+            {">",  GreaterThan},
+            {">=", GreaterEqual},
+            {"<",  LessThan},
+            {"<=", LessEqual}
+        };
+        return toOp[opStr];
+    };
+}
--- MiniDeSQL/RecordManager.cpp
+++ MiniDeSQL/RecordManager.cpp
@@ -9,10 +9,11 @@ RecordManager::RecordIterator::RecordIterator(const MINI_TYPE::TableInfo & table
     this->bm = bm;
     this->record_index = record_index;
     record_length = table.record_length + 1;
-    block_id = record_index * record_length / MINI_TYPE::BlockSize;
-    in_block_record_index = record_index * record_length - MINI_TYPE::BlockSize * block_id;
-    past_the_end_block_id = bm->PastTheEndBlockID(table.name);
     records_per_block = MINI_TYPE::BlockSize / record_length;
+    block_id = record_index / records_per_block;
+    in_block_record_index = record_index - block_id * records_per_block;
+    past_the_end_block_id = bm->PastTheEndBlockID(table.name);
+    
     block = bm->GetBlock(MINI_TYPE::TableFileName(table.name), block_id);
 }
 
@@ -27,10 +28,11 @@ bool RecordManager::RecordIterator::Read(MINI_TYPE::Record & record)
         return false;
     else
     {
+        record.Conform(table);
         int byte_offset = 1;
         for (auto & value : record.values)
         {
-            value.ReadFromMemory(block->head_pointer(), in_block_record_index * record_length + byte_offset);
+            value.ReadFromMemory(block->head_pointer(false), in_block_record_index * record_length + byte_offset);
             byte_offset += value.type.TypeSize();
         }
         return true;
@@ -41,7 +43,7 @@ void RecordManager::RecordIterator::Write(const MINI_TYPE::Record & record) cons
     int byte_offset = 1;
     for (auto & value : record.values)
     {
-        value.WriteToMemory(block->head_pointer(), in_block_record_index * record_length + byte_offset);
+        value.WriteToMemory(block->head_pointer(true), in_block_record_index * record_length + byte_offset);
         (*block)[in_block_record_index * record_length] = MINI_TYPE::NonEmpty;
         byte_offset += value.type.TypeSize();
     }
@@ -53,6 +55,7 @@ void RecordManager::RecordIterator::Delete()
 
 bool RecordManager::RecordIterator::Next(bool expand)
 {
+    past_the_end_block_id = bm->PastTheEndBlockID(table.name);
     record_index++;
     in_block_record_index++;
     if (in_block_record_index >= records_per_block)
@@ -67,24 +70,6 @@ bool RecordManager::RecordIterator::Next(bool expand)
         return true;
 }
 
-// class RecordManager
-// {
-// public:
-//     RecordManager(BufferManager *bm, IndexManager *im) : bm(bm), im(im) {}
-//     bool CreateTableFile(const MINI_TYPE::TableInfo & table);
-//     bool DeleteTableFile(const MINI_TYPE::TableInfo & table);
-//     bool BuildIndex(const MINI_TYPE::TableInfo & table, const MINI_TYPE::Attribute & attribute);
-//     bool DropIndex(const MINI_TYPE::TableInfo & table, const MINI_TYPE::Attribute & attribute);
-//     bool InsertRecord(const MINI_TYPE::TableInfo & table, const MINI_TYPE::Record & record);
-//     MINI_TYPE::Table SelectRecord(const MINI_TYPE::TableInfo & table, \
-//             const std::vector<MINI_TYPE::Condition> & conditions);
-//     MINI_TYPE::Table SelectRecord(const MINI_TYPE::TableInfo & table, \
-//             const std::vector<MINI_TYPE::Condition> & conditions, const IndexInfo & index);
-//     bool DeleteRecord(const MINI_TYPE::TableInfo & table, const vector<Condition> & conditions);
-// private:
-//     BufferManager * bm;
-//     IndexManager * im;
-// };
 bool RecordManager::CreateTableFile(const MINI_TYPE::TableInfo & table)
 {
    bm->CreateFile(MINI_TYPE::TableFileName(table.name));
@@ -97,44 +82,42 @@ bool RecordManager::DeleteTableFile(const MINI_TYPE::TableInfo & table)
    return true;
 }
 
-bool RecordManager::BuildIndex(const MINI_TYPE::TableInfo & table, const MINI_TYPE::Attribute & attribute)
+bool RecordManager::BuildIndex(MINI_TYPE::TableInfo & table, const MINI_TYPE::Attribute & attribute)
 {
     std::string index_name = MINI_TYPE::IndexName(table.name, attribute.name);
+    table.indices[attribute.name] = index_name;
     im->CreateIndex(index_name, attribute.type);
-    if(table.record_count > 0) {
-        RecordIterator iter(table, 0, bm);
-        while (true) {
-            MINI_TYPE::Record record;
-            iter.Read(record);
+    RecordIterator iter(table, 0, bm);
+    while (true)
+    {
+        MINI_TYPE::Record record;
+        if (iter.Read(record))
+        {
             MINI_TYPE::SqlValue key;
-            for (int i = 0; i < table.attributes.size(); i++) {
-                if (table.attributes[i].name == attribute.name) {
+            for (int i = 0; i < table.attributes.size(); i++)
+            {
+                if (table.attributes[i].name == attribute.name)
+                {
                     key = record.values[i];
                     break;
                 }
             }
             im->InsertKey(index_name, key, iter.CurrentIndex());
+            
         }
+        if (not iter.Next())
+            break;
     }
+    return true;
 }
-bool RecordManager::DropIndex(const MINI_TYPE::TableInfo & table, const MINI_TYPE::Attribute & attribute)
+bool RecordManager::DropIndex(MINI_TYPE::TableInfo & table, const MINI_TYPE::Attribute & attribute)
 {
     std::string index_name = MINI_TYPE::IndexName(table.name, attribute.name);
+    table.indices.erase(attribute.name);
     im->DropIndex(index_name);
     return true;
 }
-
-bool RecordManager::DropIndex(const std::string indexName) {
-    im->DropIndex(indexName);
-    return true;
-}
-
-bool RecordManager::DropIndex(const std::vector<std::string> indexNames) {
-    for (auto &indexName : indexNames)
-        im->DropIndex(indexName);
-    return true;
-}
-bool RecordManager::InsertRecord(const MINI_TYPE::TableInfo & table, const MINI_TYPE::Record & record)
+bool RecordManager::InsertRecord(MINI_TYPE::TableInfo & table, const MINI_TYPE::Record & record)
 {
     int past_the_end_block_id = bm->PastTheEndBlockID(MINI_TYPE::TableFileName(table.name));
     int start_record_index;
@@ -151,10 +134,17 @@ bool RecordManager::InsertRecord(const MINI_TYPE::TableInfo & table, const MINI_
         if (not iter.Read(temp))
         {
             iter.Write(record);
+            for (int i = 0; i < record.values.size(); i++)
+            {
+                if (table.indices.find(table.attributes[i].name) != table.indices.end())
+                    im->InsertKey(table.indices.at(table.attributes[i].name), record.values[i], iter.CurrentIndex());
+            }
             break;
         }
         iter.Next(true);
     }
+    
+    table.record_count ++;
     return true;
 }
 
@@ -177,14 +167,22 @@ MINI_TYPE::Table RecordManager::SelectRecord(const MINI_TYPE::TableInfo & table,
     return result;
 }
 MINI_TYPE::Table RecordManager::SelectRecord(const MINI_TYPE::TableInfo & table, \
-       const std::vector<MINI_TYPE::Condition> & conditions, const MINI_TYPE::IndexInfo & index)
+       const std::vector<MINI_TYPE::Condition> & conditions, const std::string & attr_using_index)
 {
     MINI_TYPE::Table result(table);
     MINI_TYPE::Condition cond_using_index;
     using MINI_TYPE::Operator;
+    if (table.indices.find(attr_using_index) == table.indices.end())
+    {
+        std::cerr << "Index on " + attr_using_index + " not found! Using select without index...\n";
+        return SelectRecord(table, conditions);
+        
+    }
+    std::string index = table.indices.at(attr_using_index);
+    
     for (auto & cond : conditions)
     {
-        if (cond.attributeName == index.name)
+        if (cond.attributeName == attr_using_index)
         {
             cond_using_index = cond;
         }
@@ -193,15 +191,15 @@ MINI_TYPE::Table RecordManager::SelectRecord(const MINI_TYPE::TableInfo & table,
     IndexManager::iterator finish;
     auto op = cond_using_index.op;
     if (op == Operator::LessThan or op == Operator::LessEqual)
-        start = im->Begin(index.name);
+        start = im->Begin(index);
     else
-        start = im->Find(index.name, cond_using_index.value);
+        start = im->Find(index, cond_using_index.value);
     if (op == Operator::GreaterThan or op == Operator::GreaterEqual)
-        finish = im->End(index.name);
+        finish = im->End(index);
     else
-        start = im->Find(index.name, cond_using_index.value);
+        finish = im->Find(index, cond_using_index.value);
     IndexManager::iterator iter;
-    for (auto iter = start; iter != finish; iter++)
+    for (iter = start; iter != finish; iter++)
     {
         auto record_fetcher = RecordIterator(table, (*iter).second, bm);
         MINI_TYPE::Record temp;
@@ -209,11 +207,39 @@ MINI_TYPE::Table RecordManager::SelectRecord(const MINI_TYPE::TableInfo & table,
             result.records.push_back(temp);
     }
     // fetch last and test
-    auto record_fetcher = RecordIterator(table, (*iter).second, bm);
-    MINI_TYPE::Record temp;
-    if (record_fetcher.Read(temp) and MINI_TYPE::Test(cond_using_index, table, temp))
-        result.records.push_back(temp);
+
+    if (iter != im->End(index))
+    {
+        auto record_fetcher = RecordIterator(table, (*iter).second, bm);
+        MINI_TYPE::Record temp;
+        if (record_fetcher.Read(temp) and MINI_TYPE::Test(cond_using_index, table, temp))
+            result.records.push_back(temp);
+    }
+    
+
+    return result.Select(conditions);
     
 }
 
-bool RecordManager::DeleteRecord(const MINI_TYPE::TableInfo & table, const vector<MINI_TYPE::Condition> & conditions){}
+bool RecordManager::DeleteRecord(MINI_TYPE::TableInfo & table, const vector<MINI_TYPE::Condition> & conditions)
+{
+    auto iter = RecordIterator(table, 0, bm);
+    while (true)
+    {
+        MINI_TYPE::Record temp;
+        if (iter.Read(temp) and MINI_TYPE::Test(conditions, table, temp))
+        {
+            iter.Delete();
+            for (int i = 0; i < temp.values.size(); i++)
+            {
+                if (table.indices.find(table.attributes[i].name) != table.indices.end())
+                    im->RemoveKey(table.indices.at(table.attributes[i].name), temp.values[i]);
+            }
+        }
+        
+        if (not iter.Next())
+            break;
+    }
+    table.record_count--;
+    return true;
+}
